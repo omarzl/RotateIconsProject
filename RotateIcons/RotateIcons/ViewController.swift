1cf7765718ea257cf0eb5de2c77902b4579ff801
omarzl <contacto@omarzl.com>
2017-07-15 17:41:01 -0500

    Generated project

---
+++ RotateIcons/RotateIcons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RotateIcons
+//
+//  Created by omarzl on 15/07/17.
+//  Copyright © 2017 omarzl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
